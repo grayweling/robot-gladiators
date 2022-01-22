a4a069e7e10ead51dd0a1fb01e378e1444f53ae9
grayweling <jy6228@yahoo.com>
2022-01-22 11:00:45 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,8 @@ var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var pickedEnemyName = enemyNames[i];
+
 console.log(enemyNames);
 console.log(enemyNames.length);
 console.log(enemyNames[0]);
@@ -26,6 +28,13 @@ var startGame = function() {
       enemyHealth = 50;
 
       fight(pickedEnemyName);
+    }
+    if (playerHealth > 0 && i<enemyNames.length - 1){
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+      if (storeConfirm) {
+        shop();
+      }
     }
     else {
       window.alert("You have lost your robot in battle! Game Over!");
@@ -50,6 +59,54 @@ var endGame = function() {
   }
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+};
+
+var shop = function() {
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  switch (shopOptionPrompt) {
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >=7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;  
+      }
+
+      else {
+        window.alert("You don't have enough money!");
+      }
+
+      break;
+
+    case "UPGRADE":
+    case "upgrade":
+      if (playerMoney >=7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;  
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+
+      break;
+
+    case "LEAVE":3
+    case "leave":
+      window.alert("Leaving the store.");
+
+      break;
+
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+
+      break;
   }
 };
 
@@ -103,7 +160,7 @@ var fight = function(enemyName) {
     if (playerHealth <= 0) {
       window.alert(playerName + ' has died!');
       // leave while() loop if player is dead
-      break;
+      endGame();
     } else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
@@ -132,7 +189,7 @@ for (var i = 0; i < enemyNames.length; i++) {
   // if player isn't alive, stop the game
   else {
     window.alert('You have lost your robot in battle! Game Over!');
-    break;
+    endGame();
   }
-}
+};
 startGame();
